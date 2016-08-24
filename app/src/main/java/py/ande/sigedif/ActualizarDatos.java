dae82a580df5fe5aaff85d08d0536785607e8d65
ArKanH3LL <felix.arkanhell@gmail.com>
2016-08-24 14:57:15 -0400

    Initial commit

---
+++ app/src/main/java/py/ande/sigedif/ActualizarDatos.java
@@ -0,0 +1,21 @@
+package py.ande.sigedif;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by asu05894 on 3/8/2016.
+ */
+public class ActualizarDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+
+    }
+}
+
---
+++ app/src/main/java/py/ande/sigedif/Navegar.java
@@ -0,0 +1,19 @@
+package py.ande.sigedif;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by asu05894 on 3/8/2016.
+ */
+public class Navegar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navegacion);
+
+    }
+
+
+}
